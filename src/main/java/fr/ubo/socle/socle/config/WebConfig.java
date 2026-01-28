fc7730fbd423b82373535f685c0342c2e8485da3
Mouaad ELHANSALI <elhansali.mouaad@etu.uae.ac.ma>
2026-01-28 15:07:20 +0100

    fix(CORS): allowing frontend communication

--- src/main/java/fr/ubo/socle/socle/config/WebConfig.java
+++ src/main/java/fr/ubo/socle/socle/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080","http://localhost")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOriginPatterns("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
